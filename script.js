d8a1e1fecd4de0b01f1de2855a0c3aedf782118a
Barba2k2 <lorenzo.dz@hotmail.com>
2023-07-03 12:24:40 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let html = document.querySelector("html");
+let checkbox = document.querySelector("#dark");
+
+checkbox.addEventListener("change", () => {
+  html.classList.toggle("dark-mode");
+});
